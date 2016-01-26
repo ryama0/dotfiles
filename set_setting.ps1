74434ec7684b2996a01b069bc284fc2d43156380
ryama <create.ryama@gmail.com>
2016-01-26 16:05:21 +0900

    [fix]Windowsのインストールスクリプトを修正 UTF8からSJISへの変換が失敗していた

--- set_setting.ps1
+++ set_setting.ps1
@@ -1,10 +1,14 @@
-.\UTF8toSJIS.ps1 -in .gvimrc -out ../_gvimrc
-.\UTF8toSJIS.ps1 -in .vimperatorrc -out ../_vimperatorrc
-.\UTF8toSJIS.ps1 -in .vimrc -out ../_vimrc
+function utf8_to_sjis ($in, $out){
+  Get-Content -Encoding UTF8 $in | Set-Content $out
+}
+
+utf8_to_sjis .gvimrc ../_gvimrc
+utf8_to_sjis .vimperatorrc ../_vimperatorrc
+utf8_to_sjis .vimrc ../_vimrc
 
 mkdir $HOME\vimfiles
 mkdir $HOME\vimfiles\user_autoload\init
-Get-ChildItem  vim_user_autoload\init | ForEach-Object {Invoke-Expression ("./UTF8toSJIS.ps1 -in vim_user_autoload/init/{0} -out $HOME/vimfiles/user_autoload/init/{1}.vim" -f ${_}.name ,${_}.BaseName)}
+Get-ChildItem  vim_user_autoload\init | ForEach-Object {Invoke-Expression ("utf8_to_sjis vim_user_autoload/init/{0} $HOME/vimfiles/user_autoload/init/{1}.vim" -f ${_}.name ,${_}.BaseName)}
 mkdir $HOME\vimfiles\user_autoload\plugins
-Get-ChildItem  vim_user_autoload\plugins | ForEach-Object {Invoke-Expression ("./UTF8toSJIS.ps1 -in vim_user_autoload/plugins/{0} -out $HOME/vimfiles/user_autoload/plugins/{1}.vim" -f ${_}.name ,${_}.BaseName)}
+Get-ChildItem  vim_user_autoload\plugins | ForEach-Object {Invoke-Expression ("utf8_to_sjis vim_user_autoload/plugins/{0} $HOME/vimfiles/user_autoload/plugins/{1}.vim" -f ${_}.name ,${_}.BaseName)}
 Copy-Item .latexmkrc ${HOME}/.latexmkrc
