bddcc529bb335c906240136343d3e73ce87e9c7a
ryama0 <ryama0@users.noreply.github.com>
2017-03-24 01:05:42 +0900

    Revert "install.shを修正"

--- install.sh
+++ install.sh
@@ -1,34 +1,29 @@
 #!/bin/bash
 
-if [ -v DOTFILES_PATH ]; then
-  DOTFILES_PATH="$(cd "$(dirname "${BASH_SOURCE:-${(%):-%N}}")"; pwd)"
-fi
-if [ -v INSTALL_PATH ]; then
-  INSTALL_PATH=${HOME}
-fi
-
+DOTFILES_PATH="$(cd "$(dirname "${BASH_SOURCE:-${(%):-%N}}")"; pwd)"
 
 #link
-ln -s $DOTFILES_PATH/.latexmkrc ${INSTALL_PATH}/.latexmkrc
-ln -s $DOTFILES_PATH/.vimperatorrc ${INSTALL_PATH}/.vimperatorrc
-ln -s $DOTFILES_PATH/.sbtconfig ${INSTALL_PATH}/.sbtconfig
+ln -s $DOTFILES_PATH/.latexmkrc ~/.latexmkrc
+ln -s $DOTFILES_PATH/.vimperatorrc ~/.vimperatorrc
+ln -s $DOTFILES_PATH/.sbtconfig ~/.sbtconfig
 
 #zsh
-ln -s $DOTFILES_PATH/.zshrc ${INSTALL_PATH}/.zshrc
-mkdir ${INSTALL_PATH}/.zsh
-ln -s $DOTFILES_PATH/zsh_user_autoload ${INSTALL_PATH}/.zsh/user_autoload
+ln -s $DOTFILES_PATH/.zshrc ~/.zshrc
+mkdir ~/.zsh
+ln -s $DOTFILES_PATH/zsh_user_autoload ~/.zsh/user_autoload
 
 #vim
-ln -s $DOTFILES_PATH/.vimrc ${INSTALL_PATH}/.vimrc
-ln -s $DOTFILES_PATH/.gvimrc ${INSTALL_PATH}/.gvimrc
-mkdir -p ${INSTALL_PATH}/.vim/bundle
-ln -s $DOTFILES_PATH/vim_user_autoload ${INSTALL_PATH}/.vim/user_autoload
-git clone git://github.com/Shougo/neobundle.vim ${INSTALL_PATH}/.vim/bundle/neobundle.vim
-ls ${INSTALL_PATH}/.vim/user_autoload/neobundle/available | while read line; do eval "ln -s ${INSTALL_PATH}/.vim/user_autoload/neobundle/available/${line} ${INSTALL_PATH}/.vim/user_autoload/neobundle/enable/${line}";done
+ln -s $DOTFILES_PATH/.vimrc ~/.vimrc
+ln -s $DOTFILES_PATH/.gvimrc ~/.gvimrc
+mkdir -p ~/.vim/bundle
+ln -s $DOTFILES_PATH/vim_user_autoload ~/.vim/user_autoload
+git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+ls ~/.vim/user_autoload/neobundle/available | while read line; do eval "ln -s ~/.vim/user_autoload/neobundle/available/${line} ~/.vim/user_autoload/neobundle/enable/${line}";done
 
 ##dict
-ln -s $DOTFILES_PATH/dict ${INSTALL_PATH}/.vim/dict
+ln -s $DOTFILES_PATH/dict ~/.vim/dict
 
 #tmux
-ln -s $DOTFILES_PATH/.tmux.conf ${INSTALL_PATH}/.tmux.conf
-ln -s $DOTFILES_PATH/.tmux.d ${INSTALL_PATH}/.tmux.d
+ln -s $DOTFILES_PATH/.tmux.conf ~/.tmux.conf
+ln -s $DOTFILES_PATH/.tmux.d ~/.tmux.d
+
