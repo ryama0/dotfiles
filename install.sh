af1d24b17468432cfec21a10faf4862c2dc383ac
ryama <create.ryama0@gmail.com>
2017-03-24 00:29:59 +0900

    install.shで配置するパスを変更

--- install.sh
+++ install.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
 
-DOTFILES_PATH="$(cd "$(dirname "${BASH_SOURCE:-${(%):-%N}}")"; pwd)"
+INSTALL_PATH="${HOME}"
 
 #link
-ln -s $DOTFILES_PATH/.latexmkrc ~/.latexmkrc
-ln -s $DOTFILES_PATH/.vimperatorrc ~/.vimperatorrc
-ln -s $DOTFILES_PATH/.sbtconfig ~/.sbtconfig
+ln -s $INSTALL_PATH/.latexmkrc ~/.latexmkrc
+ln -s $INSTALL_PATH/.vimperatorrc ~/.vimperatorrc
+ln -s $INSTALL_PATH/.sbtconfig ~/.sbtconfig
 
 #zsh
-ln -s $DOTFILES_PATH/.zshrc ~/.zshrc
+ln -s $INSTALL_PATH/.zshrc ~/.zshrc
 mkdir ~/.zsh
-ln -s $DOTFILES_PATH/zsh_user_autoload ~/.zsh/user_autoload
+ln -s $INSTALL_PATH/zsh_user_autoload ~/.zsh/user_autoload
 
 #vim
-ln -s $DOTFILES_PATH/.vimrc ~/.vimrc
-ln -s $DOTFILES_PATH/.gvimrc ~/.gvimrc
+ln -s $INSTALL_PATH/.vimrc ~/.vimrc
+ln -s $INSTALL_PATH/.gvimrc ~/.gvimrc
 mkdir -p ~/.vim/bundle
-ln -s $DOTFILES_PATH/vim_user_autoload ~/.vim/user_autoload
+ln -s $INSTALL_PATH/vim_user_autoload ~/.vim/user_autoload
 git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 ls ~/.vim/user_autoload/neobundle/available | while read line; do eval "ln -s ~/.vim/user_autoload/neobundle/available/${line} ~/.vim/user_autoload/neobundle/enable/${line}";done
 
 ##dict
-ln -s $DOTFILES_PATH/dict ~/.vim/dict
+ln -s $INSTALL_PATH/dict ~/.vim/dict
 
 #tmux
-ln -s $DOTFILES_PATH/.tmux.conf ~/.tmux.conf
-ln -s $DOTFILES_PATH/.tmux.d ~/.tmux.d
-
+ln -s $INSTALL_PATH/.tmux.conf ~/.tmux.conf
+ln -s $INSTALL_PATH/.tmux.d ~/.tmux.d
