5587606e9fc9c643dd7725d8e44a462bb1c0b4f0
ryama0 <create.ryama0@gmail.com>
2017-12-06 22:40:20 +0900

    pyenvの設定を追加

--- zsh_user_autoload/linux/basic.sh
+++ zsh_user_autoload/linux/basic.sh
@@ -53,3 +53,13 @@ if [ -f "${SSH_ENV}" ]; then
 else
   start_ssh_agent
 fi
+
+# pyenv
+if [ -e $HOME/.pyenv ]; then
+  export PYENV_ROOT="$HOME/.pyenv"
+  export PATH="${PYENV_ROOT}/bin:${PATH}"
+  if command -v pyenv 1>/dev/null 2>&1; then
+    eval "$(pyenv init -)"
+    eval "$(pyenv virtualenv-init -)"
+  fi
+fi
