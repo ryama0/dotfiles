bcc465a98fb7f88f28b24fde5e6fefa77d66fb08
ryama0 <create.ryama0@gmail.com>
2018-04-13 20:44:33 +0900

    linux package-upgradeの挙動を変更

--- zsh_user_autoload/linux/basic.sh
+++ zsh_user_autoload/linux/basic.sh
@@ -35,7 +35,7 @@ distri_name=`get_linux_distribution`
 set_linuxbrew
 case ${distri_name} in
     Ubuntu)
-        alias package-upgrade='sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y'
+        alias package-upgrade='sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y && sudo apt-get autoremove -y'
         ;;
 esac
 
