a0b54010a9f52749a7dcd23fcc541f037276444c
ryama0 <create.ryama0@gmail.com>
2018-10-21 05:40:04 +0900

    zsh pyenvの設定を修正

--- zsh_user_autoload/common/pyenv.sh
+++ zsh_user_autoload/common/pyenv.sh
@@ -1,7 +1,9 @@
 # pyenv
 if [ -d "$HOME/.pyenv" ]; then
   export PYENV_ROOT="$HOME/.pyenv"
-  export PATH="${PYENV_ROOT}/bin:${PATH}"
+  if [ -d "$PYENV_ROOT/bin" ]; then
+    export PATH="${PYENV_ROOT}/bin:${PATH}"
+  fi
   if command -v pyenv 1>/dev/null 2>&1; then
     eval "$(pyenv init -)"
     eval "$(pyenv virtualenv-init -)"
