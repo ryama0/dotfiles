a4b2d574c28fc476658a819d088049f56c5b359b
ryama0 <create.ryama0@gmail.com>
2021-09-26 03:57:49 +0900

    ビルド用環境変数を修正

--- zsh_user_autoload/osx/homebrew.profile.sh
+++ zsh_user_autoload/osx/homebrew.profile.sh
@@ -4,6 +4,7 @@ export pkg_config_path=(
     /usr/local/opt/libpq/lib/pkgconfig(N-/)
     /usr/local/opt/mysql-client/lib/pkgconfig(N-/)
     /usr/local/opt/openssl@1.1/lib/pkgconfig(N-/)
+    /usr/local/opt/readline/lib/pkgconfig(N-/)
     $pkg_config_path
 )
 
@@ -12,17 +13,15 @@ ldflag_paths=(
     /usr/local/opt/libpq/lib(N-/)
     /usr/local/opt/mysql-client/lib(N-/)
     /usr/local/opt/openssl@1.1/lib(N-/)
+    /usr/local/opt/readline/lib(N-/)
 )
 cppflag_paths=(
     /usr/local/opt/libpq/include(N-/)
     /usr/local/opt/mysql-client/include(N-/)
     /usr/local/opt/openssl@1.1/include(N-/)
+    /usr/local/opt/readline/include(N-/)
 )
 
-export ldflags=(
-    ${ldflag_paths[@]/#/-L}
-    $ldflags
-)
 export LDFLAGS="${ldflag_paths[@]/#/-L} ${LDFLAGS}"
 export CPPFLAGS="${cppflag_paths[@]/#/-I} ${CPPFLAGS}"
 
