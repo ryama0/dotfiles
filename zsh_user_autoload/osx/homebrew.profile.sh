45a5f0304389365cc50b1aadcb3fece8cd1bab0c
ryama0 <create.ryama0@gmail.com>
2023-11-29 19:28:45 +0900

    homebrew の prefix に対応

--- zsh_user_autoload/osx/homebrew.profile.sh
+++ zsh_user_autoload/osx/homebrew.profile.sh
@@ -1,28 +1,29 @@
 typeset -xT PKG_CONFIG_PATH pkg_config_path
 typeset -gU PKG_CONFIG_PATH pkg_config_path
+HOMEBREW_PREFIX=`brew --prefix`
 export pkg_config_path=(
-    /usr/local/opt/libpq/lib/pkgconfig(N-/)
-    /usr/local/opt/mysql-client/lib/pkgconfig(N-/)
-    /usr/local/opt/openssl@1.1/lib/pkgconfig(N-/)
-    /usr/local/opt/openssl@3/lib/pkgconfig(N-/)
-    /usr/local/opt/readline/lib/pkgconfig(N-/)
+    $HOMEBREW_PREFIX/opt/libpq/lib/pkgconfig(N-/)
+    $HOMEBREW_PREFIX/opt/mysql-client/lib/pkgconfig(N-/)
+    $HOMEBREW_PREFIX/opt/openssl@1.1/lib/pkgconfig(N-/)
+    $HOMEBREW_PREFIX/opt/openssl@3/lib/pkgconfig(N-/)
+    $HOMEBREW_PREFIX/opt/readline/lib/pkgconfig(N-/)
     $pkg_config_path
 )
 
 typeset -gU LDFLAGS CPPFLAGS
 ldflag_paths=(
-    /usr/local/opt/libpq/lib(N-/)
-    /usr/local/opt/mysql-client/lib(N-/)
-    /usr/local/opt/openssl@1.1/lib(N-/)
-    /usr/local/opt/openssl@3/lib(N-/)
-    /usr/local/opt/readline/lib(N-/)
+    $HOMEBREW_PREFIX/opt/libpq/lib(N-/)
+    $HOMEBREW_PREFIX/opt/mysql-client/lib(N-/)
+    $HOMEBREW_PREFIX/opt/openssl@1.1/lib(N-/)
+    $HOMEBREW_PREFIX/opt/openssl@3/lib(N-/)
+    $HOMEBREW_PREFIX/opt/readline/lib(N-/)
 )
 cppflag_paths=(
-    /usr/local/opt/libpq/include(N-/)
-    /usr/local/opt/mysql-client/include(N-/)
-    /usr/local/opt/openssl@1.1/include(N-/)
-    /usr/local/opt/openssl@3/include(N-/)
-    /usr/local/opt/readline/include(N-/)
+    $HOMEBREW_PREFIX/opt/libpq/include(N-/)
+    $HOMEBREW_PREFIX/opt/mysql-client/include(N-/)
+    $HOMEBREW_PREFIX/opt/openssl@1.1/include(N-/)
+    $HOMEBREW_PREFIX/opt/openssl@3/include(N-/)
+    $HOMEBREW_PREFIX/opt/readline/include(N-/)
 )
 
 export LDFLAGS="${ldflag_paths[@]/#/-L} ${LDFLAGS}"
@@ -30,3 +31,4 @@ export CPPFLAGS="${cppflag_paths[@]/#/-I} ${CPPFLAGS}"
 
 unset ldflag_paths
 unset cppflag_paths
+unset HOMEBREW_PREFIX
