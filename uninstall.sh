dd35579f65a8deb389af20199a737872675d198a
ryama0 <create.ryama0@gmail.com>
2024-07-07 00:25:51 +0900

    fix

--- uninstall.sh
+++ uninstall.sh
@@ -8,16 +8,24 @@ function remove_link() {
   fi
 }
 
+# latex
 remove_link ~/.latexmkrc
+# git
 remove_link ~/.gitconfig
 remove_link ~/.config/git/ignore
+# ssh
 remove_link ~/.ssh/config
+# zsh
 remove_link ~/.zshrc
 remove_link ~/.zprofile
 rm -rf ~/.zsh
+# vim
 remove_link ~/.vimrc
 remove_link ~/.gvimrc
 rm -rf ~/.vim
+# tmux
 remove_link ~/.tmux.conf
 remove_link ~/.tmux.d
+# alacritty
 remove_link ~/.config/alacritty
+remove_link ~/.alacritty.toml
